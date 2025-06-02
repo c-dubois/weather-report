ea083c62fe7447e333e7df1c662b32f72c23277e
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-06-02 11:38:06 -0700

    Implementing wave 2 (increasing/desceasing elements)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 "use strict";
+
+let currentTemp = 0;
+const increaseTempControl = document.getElementById('increaseTempControl');
+const decreaseTempControl = document.getElementById('decreaseTempControl');
+const tempValue = document.getElementById('tempValue')
+
+const updateTemp = () => {
+    tempValue.textContent = `${currentTemp}°C`
+};
+    
+increaseTempControl.addEventListener('click', () => {
+    currentTemp++;
+    updateTemp();
+});
+
+decreaseTempControl.addEventListener('click', () => {
+    currentTemp--;
+    updateTemp();
+});
+
+updateTemp();
