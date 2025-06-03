24651ba9e12ce9dc68f72bb11d6cbf8cf8fb5fd9
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-06-03 10:02:33 -0700

    Implementing temp ranges, text color, emojis  wave 2

--- src/index.js
+++ src/index.js
@@ -5,9 +5,31 @@ let currentTemp = 0;
 const increaseTempControl = document.getElementById('increaseTempControl');
 const decreaseTempControl = document.getElementById('decreaseTempControl');
 const tempValue = document.getElementById('tempValue');
+const landscape = document.getElementById('landscape');
+
+
 
 const updateTemp = () => {
     tempValue.textContent = `${currentTemp}°F`;
+
+    tempValue.className = '';
+
+    if (currentTemp >= 80){
+        tempValue.classList.add('red');
+        landscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+    } else if (currentTemp >= 70){
+        tempValue.classList.add('orange');
+        landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+    } else if (currentTemp >= 60){
+        tempValue.classList.add('yellow');
+        landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+    } else if (currentTemp >= 50){
+        tempValue.classList.add('green');
+        landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    } else {
+        tempValue.classList.add('teal');
+        landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    }
 };
     
 increaseTempControl.addEventListener('click', () => {
